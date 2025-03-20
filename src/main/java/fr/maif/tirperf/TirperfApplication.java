e7761fed15bfd7261ca536edb32d0d6b587d1cce
Omer HAMAD <omerhamad1o1@gmail.com>
2025-03-20 11:43:01 +0100

    feat: project init

---
+++ src/main/java/fr/maif/tirperf/TirperfApplication.java
@@ -0,0 +1,13 @@
+package fr.maif.tirperf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TirperfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TirperfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/maif/tirperf/TirperfApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.maif.tirperf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TirperfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
